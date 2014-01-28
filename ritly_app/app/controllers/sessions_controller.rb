3f6977aaea97c5100cce470cf5169fd13f4aa5e7
kaifius <kai.hofius@gmail.com>
2014-01-27 16:33:57 -0800

    can sign in (no authentication yet)

--- ritly_app/app/controllers/sessions_controller.rb
+++ ritly_app/app/controllers/sessions_controller.rb
@@ -2,6 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user_info = params.require(:session).permit(:email, :password)
+    user = User.find_by_email(user_info[:email])
+    redirect_to user
+  end
+
   def destroy
   end
 end
