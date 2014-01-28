bab86662b7c4d45eb6c26ad564a89ab7b9b1b6e6
kaifius <kai.hofius@gmail.com>
2014-01-27 16:18:10 -0800

    sign up and profile page added

--- ritly_app/app/controllers/users_controller.rb
+++ ritly_app/app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    new_user = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+    user = User.create(new_user)
+
+    redirect_to user
   end
 end
